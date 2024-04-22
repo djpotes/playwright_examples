727e5959ba9b2f9d0372995b646e17fa982cec41
djpotes <darwinpotes2102@live.com>
2024-04-22 16:16:26 -0500

    add tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require("@playwright/test");
 
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-  let ad2;
+  let ad3;
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
